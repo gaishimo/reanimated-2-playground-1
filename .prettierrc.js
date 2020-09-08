41dcfa521f8cee4bd3531e14411b9663538b7930
gaishimo <gaithimo@gmail.com>
2020-09-08 21:29:13 +0900

    Update prettier rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
+  parser: 'typescript',
+  printWidth: 80,
+  tabWidth: 2,
   trailingComma: 'all',
+  singleQuote: false,
+  semi: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
 };
